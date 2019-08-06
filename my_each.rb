4da9326590a310aaf4366744a6671c43ab032690
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-06 01:50:27 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (collection)
+ if block_given?
+   num = 0
+  while num < collection.length do
+   yield (collection[num]) 
+   num = num + 1
+  
+  end
 end
+return collection
+end  
+  
+  
