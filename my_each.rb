180014ec915a667496b288201daed9a2122b0824
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-06 01:26:36 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (collection)
+ if block_given?
+   i = 0
+  while i < array length do
+  yield (array[i]) 
+  i = i + 1
+  
+  end
 end
+  
+  
+  
